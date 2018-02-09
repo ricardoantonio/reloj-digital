4430539edd601221b5312a94645505a1887791ac
Ricardo Antonio <ricardoantoniomx@gmail.com>
2018-02-09 16:41:52 -0600

    Se aplicó el formato a la hora
    Se aplicó el formato a la hora, se le cambió el tamaño, color y color de fondo.
    Se cambió el color de la ventana y se le puso título.

--- reloj.py
+++ reloj.py
@@ -2,8 +2,11 @@ import tkinter as tk
 import time
 
 ROOT = tk.Tk()
-ROOT.geometry("200x100")
-lblReloj = tk.Label(ROOT, text="00:00")
+ROOT.geometry("300x100")
+ROOT.title("Hora actual")
+ROOT.configure(bg="Black")
+lblReloj = tk.Label(ROOT, text="00:00", fg="green",
+                    bg="black", font=("", 70))
 lblReloj.pack()
 
 
