0ae3146e1a0ce030081dd39550b4e13db904399f
Ricardo Antonio <ricardoantoniomx@gmail.com>
2018-02-09 22:56:18 -0600

    Se agregó el código necesario para mostrar la fecha

--- reloj.py
+++ reloj.py
@@ -1,10 +1,20 @@
 import tkinter as tk
 import time
+import locale
+
+try:
+    locale.setlocale(locale.LC_ALL, "esp")
+except:
+    locale.setlocale(locale.LC_ALL, "es_ES")
 
 ROOT = tk.Tk()
 ROOT.geometry("300x100")
 ROOT.title("Hora actual")
 ROOT.configure(bg="Black")
+
+fecha = time.strftime("%A, %d de %B de %Y")
+lblFecha = tk.Label(ROOT, text=fecha, fg="green", bg="black")
+lblFecha.pack()
 lblReloj = tk.Label(ROOT, text="00:00", fg="green",
                     bg="black", font=("", 70))
 lblReloj.pack()
